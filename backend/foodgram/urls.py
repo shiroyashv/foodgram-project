137dab6673e3bff20d08835f61e1ad4a64bba839
Sergey <thextreme.30@gmail.com>
2021-07-31 17:57:56 +0600

    fix urls

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
     path('api/', include('recipes.urls')),
 ]
--- backend/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, path
-
-urlpatterns = [
-    path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-]
