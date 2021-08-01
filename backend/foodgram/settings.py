26a81ce83db2d2c5d47a61a5cd9706f868a25288
Sergey <thextreme.30@gmail.com>
2021-08-01 13:44:31 +0600

    secret key env

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e4y!-u$-qt_+gjd_w+asc_&uf%3!aizbysye$&tx55b1l*_(p*'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'DEFAULT_PARAM')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
