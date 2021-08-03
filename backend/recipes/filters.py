8dad7c08ccab23d18510d4294ba24b5809c11a76
Sergey <thextreme.30@gmail.com>
2021-08-03 18:11:45 +0600

    try to fix filter

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,17 +1,15 @@
+from codecs import lookup
 import django_filters as filters
 
 from .models import Ingredient, Recipe, User
 
 
 class IngredientNameFilter(filters.FilterSet):
-    name = filters.CharFilter(field_name='name', method='name_starts_with')
+    name = filters.CharFilter(field_name='name', lookup_expr='istartswith')
 
     class Meta:
         model = Ingredient
         fields = ('name', 'measurement_unit')
-
-    def name_starts_with(self, queryset, slug, name):
-        return queryset.filter(name__startswith=name)
 
 
 class RecipeFilter(filters.FilterSet):
