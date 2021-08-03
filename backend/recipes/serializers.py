5fe15011b46150a564ddbaabf0770bebd56c824c
Sergey <thextreme.30@gmail.com>
2021-08-03 19:48:28 +0600

    second try to fix update

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -123,10 +123,12 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             ingredient_amount.save()
 
-        instance.image = validated_data.get('image')
-        instance.name = validated_data.get('name')
-        instance.text = validated_data.get('text')
-        instance.cooking_time = validated_data.get('cooking_time')
+        instance.image = validated_data.get('image', instance.image)
+        instance.name = validated_data.get('name', instance.name)
+        instance.text = validated_data.get('text', instance.text)
+        instance.cooking_time = validated_data.get(
+            'cooking_time', instance.cooking_time
+        )
         instance.save()
 
         return instance
